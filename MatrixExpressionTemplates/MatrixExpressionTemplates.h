b10b765b28e82f4752310850883b1a54297c9c10
Avi Korzac <avikor13@gmail.com>
2023-01-20 16:22:20 +0200

    Add project files.

---
+++ MatrixExpressionTemplates/MatrixExpressionTemplates.h
@@ -0,0 +1,320 @@
+﻿#ifndef MAT_EXPRESSION_TEMPLATES
+#define MAT_EXPRESSION_TEMPLATES
+
+
+#include <stdexcept>
+#include <utility>
+
+
+namespace met
+{
+	struct MatrixSize
+	{
+		int rows;
+		int cols;
+	};
+
+	// copy by value since sizeof(MatrixSize) is small
+	bool operator==(const MatrixSize left, const MatrixSize right)
+	{
+		return left.rows == right.rows && left.cols == right.cols;
+	}
+
+
+	class mat_expers_size_diff_exception : public std::invalid_argument
+	{
+	public:
+		mat_expers_size_diff_exception()
+			: std::invalid_argument{ "matrix expressions must have the same size." }
+		{ }
+	};
+
+
+	template <std::semiregular T, typename Expression>
+	class MatrixExpression
+	{
+	public:
+		T operator()(int row, int col) const noexcept
+		{
+			return static_cast<const Expression&>(*this)(row, col);
+		}
+
+		MatrixSize size() const noexcept
+		{
+			return static_cast<const Expression&>(*this).size();
+		}
+
+		virtual ~MatrixExpression() = default;
+	};
+
+
+	template <std::semiregular T>
+	class Matrix : public MatrixExpression<T, Matrix<T>>
+	{
+	public:
+		explicit Matrix(const MatrixSize matSize);
+		
+		template <typename Expression>
+		Matrix(const MatrixExpression<T, Expression>& matrixExpr);
+
+		template <typename Expression>
+		Matrix& operator=(const MatrixExpression<T, Expression>& matrixExpr) noexcept(false);
+
+		Matrix(const Matrix& other);
+		Matrix& operator=(const Matrix& other);
+
+		Matrix(Matrix&& other) noexcept;
+
+		Matrix& operator=(Matrix&& other) noexcept = delete;
+
+		// move assignment replacement
+		// since we throw an exception if other has different Matrixsize
+		void steal(Matrix&& other) noexcept(false);
+
+		~Matrix();
+
+		T* begin() noexcept;
+		T* end() noexcept;
+
+		[[nodiscard]] T operator()(const int i, const int j) const noexcept;
+		T& operator()(const int i, const int j) noexcept;
+
+		[[nodiscard]] MatrixSize size() const noexcept;
+
+		void fill(const T val) noexcept;
+
+		[[nodiscard]] bool isEmpty() const noexcept;
+
+	private:
+		MatrixSize matSize_;
+		T* arr_;
+
+
+		template <typename Expression>
+		void assign_from_mat_expression(const MatrixExpression<T, Expression>& matrixExpr);
+	};
+
+	template<std::semiregular T>
+	bool operator==(const Matrix<T>& left, const Matrix<T>& right)
+	{
+		const MatrixSize leftSize{ left.size() };
+		if (leftSize != right.size())
+		{
+			return false;
+		}
+
+		for (int i{ 0 }; i != leftSize.rows; ++i)
+		{
+			for (int j{ 0 }; j != leftSize.cols; ++j)
+			{
+				if (left(i, j) != right(i, j))
+				{
+					return false;
+				}
+			}
+		}
+
+		return true;
+	}
+
+	template <std::semiregular T>
+	template<typename Expression>
+	void Matrix<T>::assign_from_mat_expression(const MatrixExpression<T, Expression>& matrixExpr)
+	{
+		for (int row{ 0 }; row != matSize_.rows; ++row)
+		{
+			for (int col{ 0 }; col != matSize_.cols; ++col)
+			{
+				(*this)(row, col) = matrixExpr(row, col);
+			}
+		}
+	}
+
+	template <std::semiregular T>
+	Matrix<T>::Matrix(const MatrixSize matSize)
+		: matSize_{ matSize }
+		, arr_{ (matSize_.rows != 0 && matSize_.cols != 0) ? new T[matSize_.rows * matSize_.cols] : nullptr }
+	{ }
+
+	template <std::semiregular T>
+	template<typename Expression>
+	Matrix<T>::Matrix(const MatrixExpression<T, Expression>& matrixExpr)
+		: Matrix<T>{ matrixExpr.size() }
+	{
+		assign_from_mat_expression(matrixExpr);
+	}
+
+	template <std::semiregular T>
+	template<typename Expression>
+	Matrix<T>& Matrix<T>::operator=(const MatrixExpression<T, Expression>& matrixExpr) noexcept(false)
+	{
+		if (size() != matrixExpr.size()) [[unlikely]]
+		{
+			throw mat_expers_size_diff_exception{};
+		}
+
+		assign_from_mat_expression(matrixExpr);
+
+		return *this;
+	}
+
+	template <std::semiregular T>
+	Matrix<T>::Matrix(const Matrix& other)
+		: Matrix{ other.size() }
+	{
+		assign_from_mat_expression(other);
+	}
+
+	template <std::semiregular T>
+	Matrix<T>& Matrix<T>::operator=(const Matrix& other)
+	{
+		*this = static_cast<const MatrixExpression<T, Matrix<T>>&>(other);
+		return *this;
+	}
+
+	template <std::semiregular T>
+	Matrix<T>::Matrix(Matrix&& other) noexcept
+		: matSize_{ std::exchange(other.matSize_, std::move(MatrixSize{ 0, 0 })) }
+		, arr_{ std::exchange(other.arr_, nullptr) }
+	{ }
+
+	template <std::semiregular T>
+	void Matrix<T>::steal(Matrix&& other) noexcept(false)
+	{
+		if (size() != other.size()) [[unlikely]]
+		{
+			throw mat_expers_size_diff_exception{};
+		}
+
+		delete[] arr_;
+		arr_ = nullptr;
+		std::swap(arr_, other.arr_);
+		other.matSize_ = std::move(MatrixSize{ 0, 0});
+	}
+
+	template <std::semiregular T>
+	Matrix<T>::~Matrix()
+	{
+		delete[] arr_;
+	}
+
+	template <std::semiregular T>
+	T* Matrix<T>::begin() noexcept
+	{
+		return arr_;
+	}
+
+	template <std::semiregular T>
+	T* Matrix<T>::end() noexcept
+	{
+		return arr_ + matSize_.rows * matSize_.cols;
+	}
+
+	template <std::semiregular T>
+	T  Matrix<T>::operator()(const int i, const int j) const noexcept
+	{
+		return arr_[matSize_.cols * i + j];
+	}
+
+	template <std::semiregular T>
+	T&  Matrix<T>::operator()(const int i, const int j) noexcept
+	{
+		return arr_[matSize_.cols * i + j];
+	}
+
+	template <std::semiregular T>
+	MatrixSize Matrix<T>::size() const noexcept
+	{
+		return matSize_;
+	}
+
+	template <std::semiregular T>
+	void Matrix<T>::fill(const T val) noexcept
+	{
+		for (int& elem : *this)
+		{
+			elem = val;
+		}
+	}
+
+	template <std::semiregular T>
+	bool Matrix<T>::isEmpty() const noexcept
+	{
+		return arr_ == nullptr;
+	}
+
+	template <std::semiregular T, typename Expression>
+	class MatrixTranspose : public MatrixExpression<T, MatrixTranspose<T, Expression>>
+	{
+	public:
+		MatrixTranspose(const Expression& exper)
+			: exper_{ exper }
+		{ }
+
+		T operator()(const int row, const int col) const noexcept 
+		{ 
+			return exper_(col, row); 
+		}
+
+		MatrixSize size() const noexcept
+		{
+			MatrixSize matSize{ exper_.size() };
+			std::swap(matSize.rows, matSize.cols);
+			return matSize;
+		}
+
+
+	private:
+		const Expression& exper_;
+	};
+
+	template <std::semiregular T, typename Expression>
+	MatrixTranspose<T, Expression> transpose(const MatrixExpression<T, Expression>& matExpr)
+	{
+		return MatrixTranspose<T, Expression>{ static_cast<const Expression&>(matExpr) };
+	}
+
+
+	template <std::semiregular T, typename Expression1, typename Expression2>
+	class MatrixAdd : public MatrixExpression<T, MatrixAdd<T, Expression1, Expression2>>
+	{
+	public:
+		MatrixAdd(const Expression1& exper1, const Expression2& exper2) noexcept(false)
+			: exper1_{ exper1 }
+			, exper2_{ exper2 }
+		{
+			if (exper1_.size() != exper2_.size()) [[unlikely]]
+			{
+				throw mat_expers_size_diff_exception{};
+			}
+		}
+
+		T operator()(const int row, const int col) const noexcept
+		{ 
+			return exper1_(row, col) + exper2_(row, col); 
+		}
+
+		MatrixSize size() const noexcept
+		{ 
+			return exper1_.size(); 
+		}
+
+	private:
+		const Expression1& exper1_;
+		const Expression2& exper2_;
+	};
+
+	template <std::semiregular T, typename Expression1, typename Expression2>
+	MatrixAdd<T, Expression1, Expression2> operator+(
+		const MatrixExpression<T, Expression1>& matExpr1, 
+		const MatrixExpression<T, Expression2>& matExpr2
+		)
+	{
+		return MatrixAdd<T, Expression1, Expression2>(
+			static_cast<const Expression1&>(matExpr1), 
+			static_cast<const Expression2&>(matExpr2)
+			);
+	}
+}
+
+#endif // !MAT_EXPRESSION_TEMPLATES
---
+++ MatrixExpressionTemplates/MatrixExpressionTemplatesTests.cpp
@@ -0,0 +1,219 @@
+﻿#include "MatrixExpressionTemplates.h"
+
+
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
+
+
+TEST_CASE("size")
+{
+	constexpr met::MatrixSize matSize0{ 4, 3 };
+	met::Matrix<int> mat0{ matSize0 };
+
+	met::MatrixSize matSizeRes{ mat0.size() };
+	REQUIRE(matSize0 == matSizeRes);
+
+	met::MatrixSize matSizeRes2{ std::size(mat0) };
+	REQUIRE(matSize0 == matSizeRes2);
+}
+
+TEST_CASE("fill")
+{
+	constexpr int val{ 2 };
+
+	constexpr met::MatrixSize matSize{ 3, 4 };
+	met::Matrix<int> mat1{ matSize };
+	mat1.fill(val);
+
+	for (int i{ 0 }; i != matSize.rows; ++i)
+	{
+		for (int j{ 0 }; j != matSize.cols; ++j)
+		{
+			REQUIRE(mat1(i, j) == val);
+		}
+	}
+}
+
+TEST_CASE("isEmpty")
+{
+	constexpr met::MatrixSize zeroMatSize{ 0, 0 };
+	constexpr met::MatrixSize matSize{ 3, 4 };
+	met::Matrix<int> mat1{ matSize };
+	REQUIRE(!mat1.isEmpty());
+
+	met::Matrix<int> mat2{ zeroMatSize };
+	REQUIRE(mat2.isEmpty());
+}
+
+TEST_CASE("range-for")
+{
+	constexpr int val1{ 17 };
+	constexpr int val2{ 3 };
+	constexpr met::MatrixSize matSize{ 4, 3 };
+	met::Matrix<int> mat1{ matSize };
+	mat1.fill(val1);
+
+	for (int elem : mat1)
+	{
+		REQUIRE(elem == val1);
+	}
+
+	for (int& elem : mat1)
+	{
+		elem = val2;
+	}
+
+	for (int elem : mat1)
+	{
+		REQUIRE(elem == val2);
+	}
+}
+
+TEST_CASE("transpose && expression ctor && expression assignment")
+{
+	constexpr met::MatrixSize matSize0{ 4, 3 };
+	met::Matrix<int> mat0{ matSize0 };
+
+	int val{ 0 };
+	for (int& elem : mat0)
+	{
+		elem = val++;
+	}
+
+	auto mat0Transposed = transpose(mat0);
+	met::Matrix<int> mat0TransCopy{ mat0Transposed };
+
+	met::MatrixSize matTransposedSize{ mat0Transposed.size() };
+	REQUIRE(matTransposedSize.rows == matSize0.cols);
+	REQUIRE(matTransposedSize.cols == matSize0.rows);
+
+	for (int i{ 0 }; i != matTransposedSize.rows; ++i)
+	{
+		for (int j{ 0 }; j != matTransposedSize.cols; ++j)
+		{
+			REQUIRE(mat0Transposed(i, j) == mat0(j, i));
+			REQUIRE(mat0TransCopy(i, j) == mat0(j, i));
+		}
+	}
+
+	REQUIRE_THROWS_AS(mat0 = mat0Transposed, met::mat_expers_size_diff_exception);
+
+	auto mat0TransposedTransposed = transpose(mat0Transposed);
+
+	for (int i{ 0 }; i != matSize0.rows; ++i)
+	{
+		for (int j{ 0 }; j != matSize0.cols; ++j)
+		{
+			REQUIRE(mat0TransposedTransposed(i, j) == mat0(i, j));
+		}
+	}
+}
+
+TEST_CASE("copy ctor")
+{
+	constexpr met::MatrixSize matSize{ 4, 3 };
+	met::Matrix<int> mat1{ matSize };
+	mat1.fill(17);
+
+	met::Matrix<int> mat2{ mat1 };
+
+	REQUIRE(mat1 == mat2);
+}
+
+TEST_CASE("copy assignment")
+{
+	constexpr met::MatrixSize matSize{ 4, 3 };
+	met::Matrix<int> mat1{ matSize };
+	mat1.fill(2);
+
+	met::Matrix<int> mat2{ matSize };
+	mat2.fill(15);
+
+	REQUIRE(mat1 != mat2);
+
+	mat2 = mat1;
+
+	REQUIRE(mat1 == mat2);
+
+	constexpr met::MatrixSize matSize3{ 9, 10 };
+	met::Matrix<int> mat3{ matSize3 };
+
+	REQUIRE_THROWS_AS(mat1 = mat3, met::mat_expers_size_diff_exception);
+	REQUIRE_THROWS_AS(mat3 = mat1, met::mat_expers_size_diff_exception);
+}
+
+TEST_CASE("move ctor")
+{
+	constexpr int val{ 13 };
+	constexpr met::MatrixSize matSize{ 4, 3 };
+
+	met::Matrix<int> mat1{ matSize };
+	mat1.fill(val);
+
+	met::Matrix<int> mat2{ std::move(mat1) };
+
+	REQUIRE(mat1.isEmpty());
+	REQUIRE(mat2.size() == matSize);
+
+	for (int elem : mat2)
+	{
+		REQUIRE(elem == val);
+	}
+}
+
+TEST_CASE("steal")
+{
+	constexpr int mat1Val{ 2 };
+	constexpr int mat2Val{ 15 };
+	constexpr met::MatrixSize matSize{ 4, 3 };
+	met::Matrix<int> mat1{ matSize };
+	mat1.fill(mat1Val);
+
+	met::Matrix<int> mat2{ matSize };
+	mat2.fill(mat2Val);
+
+	REQUIRE(mat1 != mat2);
+
+	mat2.steal(std::move(mat1));
+
+	REQUIRE(mat1.isEmpty());
+
+	for (int elem : mat2)
+	{
+		REQUIRE(elem == mat1Val);
+	}
+
+	constexpr met::MatrixSize matSize3{ 7, 8 };
+	met::Matrix<int> mat3{ matSize3 };
+
+	REQUIRE_THROWS_AS(mat3.steal(std::move(mat2)), met::mat_expers_size_diff_exception);
+
+	REQUIRE(!mat2.isEmpty());
+}
+
+TEST_CASE("operator+")
+{
+	constexpr met::MatrixSize matSize{ 3, 4 };
+	met::Matrix<int> mat1{ matSize };
+	mat1.fill(2);
+
+	met::Matrix<int> mat2{ matSize };
+	mat2.fill(6);
+
+	auto view = mat1 + mat1 + mat2;
+	met::MatrixSize viewSize{ view.size() };
+
+	for (int i{ 0 }; i != viewSize.rows; ++i)
+	{
+		for (int j{ 0 }; j != viewSize.cols; ++j)
+		{
+			REQUIRE(view(i, j) == mat1(i, j) + mat1(i, j) + mat2(i, j));
+		}
+	}
+
+	constexpr met::MatrixSize matSize2{ 8, 7 };
+	met::Matrix<int> mat13{ matSize2 };
+	mat13.fill(16);
+
+	REQUIRE_THROWS_AS(mat1 + mat13, met::mat_expers_size_diff_exception);
+}
